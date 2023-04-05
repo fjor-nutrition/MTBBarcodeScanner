cd568e79f090b114b7f278d83c9245699200d738
Vadim Temnogrudov <vtemnogrudov@ya.ru>
2023-04-05 11:36:17 +0300

    Target rename

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "M13BadgeView",
+    name: "MTBBarcodeScanner",
     platforms: [
         .iOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "M13BadgeView",
-            targets: ["M13BadgeView"]),
+            name: "MTBBarcodeScanner",
+            targets: ["MTBBarcodeScanner"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "M13BadgeView",
+            name: "MTBBarcodeScanner",
             dependencies: [],
             path: "./Classes"),
     ]
